f0d0c4f99f88ea4c98e61102f8c31c78e98012ce
Admin <jksalagundi@users.noreply.github.com>
2024-01-22 04:48:22 -0600

    Updating form details

--- eod_prime/src/panels/components/LeftSidePanel.js
+++ eod_prime/src/panels/components/LeftSidePanel.js
@@ -2,6 +2,8 @@ import React, {useState} from "react";
 import { InputText } from "primereact/inputtext";
 import { InputNumber } from "primereact/inputnumber";
 import { SelectButton } from "primereact/selectbutton";
+import {useDispatch} from "react-redux";
+import {updateHeaderLeft} from "../../redux/reducers/formSlice";
 
 export const LeftSidePanel = () => {
     const options = ["AM Shift", "PM Shift"];
@@ -9,6 +11,7 @@ export const LeftSidePanel = () => {
     const [shiftLead, setShiftLead] = useState('');
     const [cashBox, setCashBox] = useState(0);
     const [gameSold, setGameSold] = useState(0);
+    const dispatch = useDispatch();
     return (
         <div className="card w-5 flex flex-column m-1 ">
             <div className="card flex flex-row justify-content-end px-4 py-2">
@@ -16,31 +19,48 @@ export const LeftSidePanel = () => {
                     options={options}
                     value={shift}
                     tooltip="Which shift are you reporting ?"
-                    onChange={(e) => setShift(e.value)} />
+                    onChange={(e) => {
+                        setShift(e.value);
+                        dispatch(updateHeaderLeft({ shift: e.value, shift_lead: shiftLead,
+                            cash_in_box: cashBox, games_sold: gameSold }))
+                    }} />
             </div>
             <div className="card flex flex-row justify-content-end px-4 py-2 mt-2">
                 <span className="p-float-label">
-                    <InputText id="shiftlead" 
+                    <InputText id="shiftlead"
                         key="shift_lead_input"
                         autoFocus="autoFocus"
                         value={shiftLead}
-                        onChange={(e)=>setShiftLead(e.target.value)}/>
+                        onChange={(e)=> {
+                            setShiftLead(e.target.value)
+                            dispatch(updateHeaderLeft({ shift: shift, shift_lead: e.target.value,
+                                cash_in_box: cashBox, games_sold: gameSold }))
+
+                        }}/>
                     <label htmlFor="shiftlead">Shift Lead</label>
                 </span>
             </div>
             <div className="card flex flex-row justify-content-end px-4 py-2 mt-3">
                 <span className="p-float-label">
-                    <InputNumber id="cashBox" value={cashBox} 
+                    <InputNumber id="cashBox" value={cashBox}
                             className="p-inputtext-normal"
-                               onValueChange={(e) => setCashBox(e.target.value)} />
+                               onValueChange={(e) => {
+                                   setCashBox(e.target.value)
+                                   dispatch(updateHeaderLeft({ shift: shift, shift_lead: shiftLead,
+                                       cash_in_box: e.target.value, games_sold: gameSold }))
+                               }} />
                     <label htmlFor="cashBox">Cash in the Box</label>
                 </span>
             </div>
             <div className="card flex flex-row justify-content-end px-4 py-2 mt-4">
                 <span className="p-float-label">
-                    <InputNumber id="gamesSold" value={gameSold} 
+                    <InputNumber id="gamesSold" value={gameSold}
                             className="p-inputtext-normal"
-                               onValueChange={(e) => setGameSold(e.target.value)} />
+                               onValueChange={(e) => {
+                                   setGameSold(e.target.value)
+                                   dispatch(updateHeaderLeft({ shift: shift, shift_lead: shiftLead,
+                                       cash_in_box: cashBox, games_sold: e.target.value}))
+                               }} />
                     <label htmlFor="gamesSold">Games Sold</label>
                 </span>
             </div>
--- eod_prime/src/redux/reducers/formSlice.js
+++ eod_prime/src/redux/reducers/formSlice.js
@@ -2,7 +2,16 @@ import {createSlice} from "@reduxjs/toolkit";
 import _ from "lodash";
 const initialState = {
     selectedLocation: null,
-    eod_report_header : {},
+    eod_report_header : {
+        shift: "AM",
+        traffic_status: "",
+        shift_lead: "",
+        location_cleaned_status: "",
+        games_sold: 0,
+        cash_in_box: 0,
+        walkins_declined: 0,
+        inventory_reorder: ''
+    },
     eod_report_details: [],
     eod_notes: null
 }
@@ -24,8 +33,19 @@ const formSlice = createSlice({
         changeLocation : (state, action) => {
             state.selectedLocation = action.payload;
         },
-        updateHeader : (state, action) => {
-            state.eod_report_header = _.cloneDeep(action.payload)
+        updateHeaderLeft : (state, action) => {
+            let payload = action.payload;
+            state.eod_report_header.shift = payload.shift;
+            state.eod_report_header.shift_lead = payload.shift_lead;
+            state.eod_report_header.cash_in_box= payload.cash_in_box;
+            state.eod_report_header.games_sold = payload.games_sold;
+        },
+        updateHeaderRight : (state, action) => {
+            let payload = action.payload;
+            state.eod_report_header.traffic_status = payload.traffic_status;
+            state.eod_report_header.location_cleaned_status = payload.location_cleaned_status;
+            state.eod_report_header.walkins_declined = payload.walkins_declined;
+            state.eod_report_header.inventory_reorder = payload.inventory_reorder;
         },
         updateDetails : (state, action) => {
             if (state.eod_report_details && state.eod_report_details.length > 0){
@@ -41,5 +61,6 @@ const formSlice = createSlice({
     }
 });
 
-export const {changeLocation, updateHeader, updateNotes, updateDetails, initiateGameStatus} = formSlice.actions;
+export const {changeLocation, updateNotes, updateHeaderLeft, updateHeaderRight,
+                updateDetails, initiateGameStatus} = formSlice.actions;
 export default formSlice.reducer;

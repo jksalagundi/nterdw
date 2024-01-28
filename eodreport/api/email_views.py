c148697e353fa61e9cbd5982aa88af2d25ca8c7e
Jay Salagundi <jksalagundi@gmail.com>
2024-01-28 06:28:24 -0600

    Work started on email sending

--- eodreport/api/email_views.py
+++ eodreport/api/email_views.py
@@ -3,6 +3,11 @@ from rest_framework.response import Response
 from rest_framework import status
 from eodreport.models import EODConfig, EndOfDayReport
 from masters.models import Location, Games
+from datetime import datetime
+
+
+class ShiftException(Exception):
+    pass
 
 
 class SendEmails(APIView):
@@ -15,10 +20,26 @@ class SendEmails(APIView):
         """
         try:
             location = Location.objects.get(pk=location_id)
+            rdate = datetime.strptime(report_date, "%Y-%m-%d")
             msg = (f" Will be sending emails for {location_id} / {location.location_name}, "
-                   f"on {report_date} for shift {shift}")
+                   f"on {report_date} / for shift {shift}")
+            if shift[:2].upper() not in (["AM", "PM"]):
+                raise ShiftException(f"Invalid shift data {shift}")
+            eod_reports = EndOfDayReport.objects.filter(report_date__date=rdate)
+            print(f"Got {eod_reports.count()} records with date")
+            eod_reports = eod_reports.filter(location=location).filter(shift=shift)
+            print(f"Got {eod_reports.count()} records with location & shift")
             print(msg)
             return Response(status=status.HTTP_200_OK, data={"status": msg})
+        except ShiftException:
+            msg = f" Exception occurred while generating emails. Invalid shift value {shift}. Should only be AM/PM "
+            print(msg)
+            return Response(status=status.HTTP_400_BAD_REQUEST, data={'error': msg})
+        except ValueError:
+            msg = (f" Exception occurred while generating emails. Invalid report date format {report_date}. "
+                   f"Expect YYYY-MM-DD")
+            print(msg)
+            return Response(status=status.HTTP_400_BAD_REQUEST, data={'error': msg})
         except Location.DoesNotExist:
             msg = f" Exception occurred while generating emails. Invalid location sent{location_id}"
             print(msg)
